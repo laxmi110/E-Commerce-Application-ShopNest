6ba6d1ba28a5f3288a800f843ee1fe2a8928a350
laxmi110 <klaxmi1196@gmail.com>
2026-08-03 21:32:16 +0530

    Update analyticsroutes.js

--- backend/routes/analyticsroutes.js
+++ backend/routes/analyticsroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAdminStats } = require('../controller/analyticalController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authmiddleware');
 const { admin } = require('../middleware/adminMiddleware');
 
 const router = express.Router();
